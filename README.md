3064afa36cb946441e8aba98661309a222438fca
Massimo Nocentini <massimo.nocentini@gmail.com>
2020-12-31 11:24:58 +0100

    Initial content

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pharodomain
 A Sphinx domain for Pharo Smalltalk.
+
+According to https://packaging.python.org/tutorials/packaging-projects/, to package me do the following steps:
+- create a virtual env and activate it
+- run `pip install --upgrade setuptools wheel`
+- run `python setup.py sdist bdist_wheel`
+
+For now we don't wont to publish it on the global PIP, therefore to install it locally run `pip install dist/`.
---
+++ setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sphinxcontrib-pharodomain", # Replace with your own username
+    version="0.0.1",
+    author="Massimo Nocentini",
+    author_email="massimo.nocentini@gmail.com",
+    description="A Sphinx domain for Pharo Smalltalk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/massimo-nocentini/pharodomain",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
